094d2d330e63ca6b02297dc54472e6b08968d925
Artistic Phoenix <artisticphoenix@gmail.com>
2018-09-08 22:27:26 -0400

    init

---
+++ src/evo/exception/EvoExceptionInterface.php
@@ -0,0 +1,18 @@
+<?php
+namespace evo\exception;
+
+/**
+ * (eJinn Generated File, do not edit directly)
+ * Exception handler
+ *
+ * @author ArtisticPhoenix
+ * @package Evo
+ * @subpackage Evo
+ * @link https://github.com/ArtisticPhoenix/Evo/issues
+ * @version 1.0.0
+ * @eJinn:buildVersion 1.0.0
+ * @eJinn:buildTime 1536450082.3241
+ */
+interface EvoExceptionInterface
+{
+}
